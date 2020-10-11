6be5687606b92168a161a37e3a472e2e5adcd6f7
Ref434 <60964571+Ref434@users.noreply.github.com>
2020-10-12 01:01:45 +0300

    Update FilterRed.cpp

--- FilterRed.cpp
+++ FilterRed.cpp
@@ -9,17 +9,16 @@ void FilterRed::action(png_toolkit *png)
 	for (int i = 0; i < imgData.compPerPixel*imgData.w*imgData.h; i += 3)
 	{
 		coord_i += 3;
-		if (coord_i >= imgData.w*imgData.compPerPixel)
-		{
-			coord_j += 1;
-			coord_i = 0;
-		}
-		if (coord_i >= coordFilter.l*imgData.compPerPixel && coord_j >= coordFilter.u && coord_i<=coordFilter.r*imgData.compPerPixel && coord_j<=coordFilter.b)
-		{
+		//if (coord_i >= imgData.w*imgData.compPerPixel)
+		//{
+		//	coord_j += 1;
+		//	coord_i = 0;
+		//}
+		//if (coord_i >= coordFilter.l*imgData.compPerPixel && coord_j >= coordFilter.u && coord_i<=coordFilter.r*imgData.compPerPixel && coord_j<=coordFilter.b)
+		//{
 			*(imgData.pixels + i) = 255;
 			*(imgData.pixels + i + 1) = 0;
 			*(imgData.pixels + i + 2) = 0;
-		}
+		//}
 	}
-	cout << coordFilter.u << " " << coordFilter.l << " " << coordFilter.b << " " << coordFilter.r << " " << endl;
 }
