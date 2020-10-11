121fcd415b7ec385135ed58f15aa37df39a7a4f1
Ref434 <60964571+Ref434@users.noreply.github.com>
2020-10-12 01:41:49 +0300

    Add files via upload

--- FilterRed.cpp
+++ FilterRed.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void FilterRed::action(png_toolkit *png)
 {
 	image_data imgData = png->getPixelData();
-	int coord_i = 0, coord_j = 0;
+	/*int coord_i = 0, coord_j = 0;
 	for (int i = 0; i < imgData.compPerPixel*imgData.w*imgData.h; i += 3)
 	{
 		coord_i += 3;
@@ -19,6 +19,14 @@ void FilterRed::action(png_toolkit *png)
 			*(imgData.pixels + i) = 255;
 			*(imgData.pixels + i + 1) = 0;
 			*(imgData.pixels + i + 2) = 0;
+		}
+	}*/
+	for (int i = coordFilter.u; i < coordFilter.b; ++i) {
+		for (int j = coordFilter.l; j < coordFilter.r; ++j) {
+			int ptr = (i*imgData.w + j)*imgData.compPerPixel;
+			imgData.pixels[ptr + 0] = (unsigned char)255;
+			imgData.pixels[ptr + 1] = (unsigned char)0;
+			imgData.pixels[ptr + 2] = (unsigned char)0;
 		}
 	}
 }
